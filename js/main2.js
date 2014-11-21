31e5814fe8202ae7272f8b10acee056a7f018560
tocute <tocute520@gmail.com>
2014-11-21 12:18:20 +0800

    add index2

---
+++ js/main2.js
@@ -0,0 +1,96 @@
+$(document).on("mobileinit",function(e)
+{
+    
+});
+
+$(document).on("pageinit","#page-1",function(e)
+{
+    Parse.initialize("CPFQXuoHErkxiN8b3uDFuGuGBnZbLen9jglvAB4p", "qvHqLCDitrqkwAL3bSbMfbdcYlUY9wlfVJZmom3S");
+    var mDistrictIDArray = [6301200,6301100,6301000,6300900,6300100,6300200,6300400,6300600,6300500,6300700,6300300,6300800];
+    var mDistrictNameArray = ["北投","士林","內湖","南港","松山","信義","中山","大同","中正","萬華","大安","文山"];
+    var mTimerInterval = 30000;
+    var getDistrictIndex = function (id)
+    {
+        var index = -1;
+        for(var i = 0; i<mDistrictIDArray.length; i++)
+        {
+            if(mDistrictIDArray[i] == id)
+            {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    var addTable = function(col)
+    {
+        var table = $("#bigTableLeft");
+        var table_id = "myTableLeft"+col;
+        if(col%2 == 1)
+        {
+            table = $("#bigTableRight");
+            table_id = "myTableRight"+col;
+        } 
+        var table_bg_color = "<colgroup><col style=background-color:#d1d1d1><col style=background-color:#ccecf2><col style=background-color:#f9f8f2></colgroup>";
+        var table_header = "<thead><tr><th>行政區</th> <th>連勝文</th> <th>柯文哲</th> </tr></thead>";
+        table.append("<td  valign=top> <table id="+table_id+" style='border:1px #2e565b solid;padding:5px;' rules='all' cellpadding='5';>"+table_bg_color+table_header+"<tbody></tbody></table></td>");
+        return table_id;
+    }
+
+    var onBtnGetAllTicketClick = function (e)
+    {
+        $.mobile.loading( 'show', {
+            text: '資料更新中...',
+            textVisible: true,
+            theme: 'z',
+            html: ""
+        });
+
+
+        Parse.Cloud.run("GetAllDistrictTicket",null, 
+        {
+            success: function(results) 
+            {
+                var totalCandidate7Tictet = 0;
+                var totalCandidate6Tictet = 0;
+            
+                for(var i = 0; i<results.length; i++)
+                {
+                    var table_element = $("#"+mLeftTableID);
+                    if(i%2 == 1)
+                        table_element = $("#"+mRightTableID);
+
+                    var entiry = results[i];
+
+                    var candidate7Tictet = entiry.candidate7;
+                    var candidate6Tictet = entiry.candidate6;
+                    var districtId = entiry.districtId;
+
+                    totalCandidate7Tictet += candidate7Tictet;
+                    totalCandidate6Tictet += candidate6Tictet;
+
+                    //+ 'tr:last'
+                    table_element.append('<tr><td>'+mDistrictNameArray[getDistrictIndex(districtId)]+'區</td><td>'+candidate6Tictet+'</td><td>'+candidate7Tictet+'</td></tr>');
+                }
+                table_element = $("#bigTable");
+                table_element.append('<tr><td><b>總票</b></td><td><b>'+totalCandidate6Tictet+'</b></td><td><b>'+totalCandidate7Tictet+'</b></td></tr>');
+                $.mobile.loading( 'hide');
+            },
+            error: function(error) 
+            {
+                /*{
+                    "code":141,
+                    "message":"密碼錯誤  請再次確認投票所編號與密碼"
+                }*/
+                alert(JSON.stringify(error.message));
+                $.mobile.loading( 'hide');
+            }
+        });  
+    }
+    var mLeftTableID = addTable(0);
+    var mRightTableID = addTable(1);
+    var mUpdateTimer = window.setInterval(onBtnGetAllTicketClick, mTimerInterval);
+    onBtnGetAllTicketClick();
+    
+});
